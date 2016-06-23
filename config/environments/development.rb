0c8d39d26c22e9ce534fa6ef7dbb694b24278f50
DrBund <stevenlammers12@gmail.com>
2016-06-23 08:23:52 -0600

    Ch 6 complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # SL: Whitelist 10.0.2.2 to prevent errors when using raing and vagrant
+  class Application < Rails::Application
+      config.web_console.whitelisted_ips = '10.0.2.2'
+  end
 end
