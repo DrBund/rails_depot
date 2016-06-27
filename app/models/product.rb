574a4de08a9086601f7ecd468a6a49f6fdf1fbd2
UCDBioe <steven.lammers@UCDenver.edu>
2016-06-27 09:22:50 -0600

    Chapter 8 complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  # Test if something has changed since last rendering
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # See how your routes lay out with "rate routes"
+  #
+  # # You can have the root of your site routed with "root"
+  root 'store#index', as: 'store'
 end
