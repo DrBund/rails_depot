0439e36f8d8234cd910adcc96385dcdd02810563
DrBund <stevenlammers12@gmail.com>
2016-06-27 15:59:48 -0600

    Chapter 9 done

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+# SL add gem to allow for "assigns" to be used
+gem 'rails-controller-testing'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '>= 5.0.0.rc1', '< 5.1'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   # Test if something has changed since last rendering
   def self.latest
     Product.order(:updated_at).last
@@ -11,4 +15,16 @@ class Product < ApplicationRecord
     with:     %r{\.(gif|jpg|png)\Z}i,
     message:  'must be a URL for GIF, JPG, or PNG image.'
   }
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items Present')
+      return false
+    end
+  end
 end
